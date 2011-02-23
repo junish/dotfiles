1522cdda4f00d129e134673e2b840ebfe03c2719
Junichi Shinohara <tabe1ahds@gmail.com>
2011-02-24 03:27:09 +0900

    vim-shellの削除にともなう修正

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,6 +4,6 @@
 ~/dotfiles/bin/setup-yum.sh
 /bin/mv -f ~/.vim ~/.vim.orig
 ~/dotfiles/bin/vim-update-bundles
-pushd ~/.vim/bundle/vimproc/
-make -f make_gcc.mak
-popd
+#pushd ~/.vim/bundle/vimproc/
+#make -f make_gcc.mak
+#popd
